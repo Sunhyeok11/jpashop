32d701e255b94eb570d1e7efb771d9f58235986b
SunHyeok <cghhld@naver.com>
2024-03-06 22:42:29 +0900

    엔티티 클래스 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+
+    private String Street;
+
+    private String zipcode;
+}

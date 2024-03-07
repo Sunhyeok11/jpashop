69cc0a41af3384f4b099e3db8bd264fe6e5a3070
SunHyeok <cghhld@naver.com>
2024-03-07 17:59:18 +0900

    엔티티 설계시 주의점

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,13 @@ public class Address {
     private String Street;
 
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        Street = street;
+        this.zipcode = zipcode;
+    }
 }

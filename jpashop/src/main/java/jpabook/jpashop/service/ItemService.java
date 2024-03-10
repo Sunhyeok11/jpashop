0b54329bb8a490038710d419858bf61f83cb9e07
SunHyeok <cghhld@naver.com>
2024-03-10 20:56:41 +0900

    상품 수량 수정

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -22,11 +22,11 @@ public class ItemService {
     }
 
     @Transactional
-    public void updateItem(Long itemId,String name, int price, int stockquantity) {
+    public void updateItem(Long itemId,String name, int price, int stockQuantity) {
         Item findItem = itemRepository.findOne(itemId);
         findItem.setName(name);
         findItem.setPrice(price);
-        findItem.setStockQuantity(stockquantity);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
